949e2c5d567f4687015fad63d3f082659ed71bbf
Chris <chris@greenhost.nl>
2018-09-03 18:18:13 +0200

    Fix escaped html.

--- lib/middleman-breadcrumbs/breadcrumbs.rb
+++ lib/middleman-breadcrumbs/breadcrumbs.rb
@@ -9,7 +9,7 @@ class Breadcrumbs < Middleman::Extension
 
   option :separator, ' > ', 'Default separator between breadcrumb levels'
   option :wrapper, nil, 'Name of tag (as symbol) in which to wrap each breadcrumb level, or nil for no wrapping'
-  option :img_attr, nil, 'Attributes for breadcrumb images.'
+  option :img_attrs, {}, 'Attributes for breadcrumb images.'
 
   expose_to_template :breadcrumbs
 
@@ -17,23 +17,24 @@ class Breadcrumbs < Middleman::Extension
     super
     @separator = options.separator
     @wrapper = options.wrapper
-    @img_attr = options.img_attr
+    @img_attrs = options.img_attrs
   end
 
-  def breadcrumbs(current_page, separator: @separator, wrapper: @wrapper, img_att: @img_attr)
+  def breadcrumbs(current_page, separator: @separator, wrapper: @wrapper, img_attrs: @img_attrs)
     hierarchy = [current_page]
     hierarchy.unshift hierarchy.first.parent while hierarchy.first.parent
     hierarchy.map { |page|
+      # Get title from the breadcrumb section in frontmatter or from the page
       title = page.data.breadcrumb.title rescue page.data.title
-      img =  page.data.breadcrumb.img rescue ""
+      # If title if false set it to an empty string
+      title = title || ""
+      # Get an image from the breadcrumb section in frontmatter if set
+      img = page.data.breadcrumb.img rescue nil
       if img
-        img = tag(:img, :src => img, **img_attr)
+        img = tag(:img, :src => img, **img_attrs)
       end
-      title = "#{img}#{title}" % {
-        :img => img,
-        :title => title
-      }
-      wrap link_to(title, page.url), wrapper: wrapper
+      link = "#{img}#{title}"
+      wrap link_to(page.url) { mark_safe(link) }, wrapper: wrapper
     }.join(h separator)
   end
 
